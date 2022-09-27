27a63811bb43cbdf44e2a5383255871757685fb3
solmoonkang <thfans0521@naver.com>
2022-09-28 01:26:51 +0900

    2022.09.26

--- src/main/java/com/example/crud_project/controller/MemberController.java
+++ src/main/java/com/example/crud_project/controller/MemberController.java
@@ -15,18 +15,20 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    // @RequestBody - HTTP로 받은 요청의 body 부분을 통채로 전달
     @PostMapping("/signUp")
-    public void signUp(SignUpDto signUpDto) {
+    public void signUp(@RequestBody SignUpDto signUpDto) {
         memberService.signUp(signUpDto);
     }
 
     @PutMapping("/update")
-    public void update(UpdateDto updateDto) {
+    public void update(@RequestBody UpdateDto updateDto) {
         memberService.update(updateDto);
     }
 
-    @GetMapping("")
-    public ReadDto read(String identity) {
+    // @RequestParam - URL에서 파라미터 추출
+    @GetMapping("") // URL에 파라미터의 값과 이름을 함께 전달(노출)
+    public ReadDto read(@RequestParam("identity") String identity) {
         return memberService.getMember(identity);
     }
 
